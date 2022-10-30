1a78b5cd1e6bd50ea964eb7e7228bf81270dd5c1
Sangeeta Dagar <sangeeta_dagar@epam.com>
2022-10-30 21:07:15 +0530

    task-5-s3-integration

---
+++ import-service/src/functions/importProducts/handler.ts
@@ -0,0 +1,40 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+import schema from './schema';
+import { PutObjectCommand, S3Client }
+  from "@aws-sdk/client-s3";
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const importProductsFile: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  try {
+    const { name } = event.queryStringParameters;
+    const clientParams = {
+      region: 'us-west-2',
+      apiVersion: "2006-03-01",
+      accessKeyId: 'AKIAUJTYHMTUIWAW3FIG',
+      secretAccessKey: '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',
+      signatureVersion: "v4",
+    };
+    const client = new S3Client(clientParams);
+    const key = name || null;
+    if (!key) {
+      throw new Error("key not defined");
+    }
+    const getObjectParams = {
+      Bucket: 's3-integration-task-5',
+      Key: `uploaded/${key}`,
+      ContentType: 'text/csv'
+    }
+    const command = new PutObjectCommand(getObjectParams);
+    const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+    return formatJSONResponse({
+      url
+    });
+  }
+  catch (e) {
+    return formatJSONResponse({ ...e, err: true });
+  }
+};
+
+export const main = middyfy(importProductsFile);
--- import-service/src/libs/lambda.ts
+++ import-service/src/libs/lambda.ts
@@ -4,3 +4,4 @@ import middyJsonBodyParser from "@middy/http-json-body-parser"
 export const middyfy = (handler) => {
   return middy(handler).use(middyJsonBodyParser())
 }
+
