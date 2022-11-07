aa3bbd7b380bb8cf15df2b9dec14020732f4f12b
Sangeeta Dagar <sangeeta_dagar@epam.com>
2022-11-08 01:41:18 +0530

    integrate-sqs-sns

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,42 +1,51 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
 import schema from './schema';
 import * as AWS from 'aws-sdk';
-import * as csvParser from 'csv-parser';
+const csv = require('csvtojson');
+const s3 = new AWS.S3({ region: process.env.REGION });
+const sqs = new AWS.SQS({ region: process.env.REGION });
 
-const s3 = new AWS.S3({ region: 'us-west-2' });
 const importFileParser: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  console.log('importFileParser...');
+  let statusCode = 200;
+  let body = {};
+
+  const Bucketparams = {
+    Bucket: process.env.BUCKET,
+    Prefix: 'uploaded/',
+  };
+
   try {
-    const { name } = event.queryStringParameters;
-    const key = name || null;
-    if (!key) {
-      throw new Error("key not defined");
+    const s3Response = await s3.listObjectsV2(Bucketparams).promise();
+    const csvFiles = (s3Response.Contents ?? []).filter(file => file.Size);
+    for (const file of csvFiles) {
+      console.log('File key...', file.Key);
+      const paramsForFile = {
+        Bucket: process.env.BUCKET,
+        Key: `${file.Key}`
+      }
+      const s3Stream = s3.getObject(paramsForFile).createReadStream();
+      const json = await csv().fromStream(s3Stream);
+      console.log(json);
+      const result = await sqs.sendMessage({
+        QueueUrl: process.env.QUEUE_URL,
+        MessageBody: JSON.stringify(json)
+
+      }).promise();
+      console.log('Result::', JSON.stringify(result));
     }
-    const getObjectParams = {
-      Bucket: 's3-integration-task-5',
-      Key: `uploaded/${key}`,
-      ContentType: 'text/csv'
-    }
-    const s3Stream = await s3.getObject(getObjectParams).createReadStream();
-    s3Stream
-      .pipe(csvParser()).on('data', (data) => {
-        console.log(data);
-      }).on('error', (e) => {
-        return formatJSONResponse({
-          e,
-          err: true
-        });
-      }).on('end', (data) => {
-        return formatJSONResponse({
-          message: `Hello welcome to the exciting Serverless world!`,
-          data,
-        });
-      });
+  } catch (err) {
+    console.error('Error appears:');
+    console.error(err);
+    statusCode = 500;
+    body = err;
   }
-  catch (e) {
-    return formatJSONResponse({ ...e, err: true });
-  }
+  return formatJSONResponse({
+    statusCode,
+    body,
+    event,
+  });
 };
 
-export const main = middyfy(importFileParser);
+export const main = importFileParser;
